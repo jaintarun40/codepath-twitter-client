afe0a4c4517f9b2a94020ec413c5d90d35005fa0
Lori Lee <leelorik@gmail.com>
2015-02-19 21:51:48 -0800

    Added rest endpoint calls, renamed stuff

---
+++ app/src/main/java/com/codepath/apps/twitterclient/TwitterClient.java
@@ -0,0 +1,49 @@
+package com.codepath.apps.twitterclient;
+
+import org.scribe.builder.api.Api;
+import org.scribe.builder.api.TwitterApi;
+
+import android.content.Context;
+
+import com.codepath.oauth.OAuthBaseClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/*
+ * 
+ * This is the object responsible for communicating with a REST API. 
+ * Specify the constants below to change the API being communicated with.
+ * See a full list of supported API classes: 
+ *   https://github.com/fernandezpablo85/scribe-java/tree/master/src/main/java/org/scribe/builder/api
+ * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
+ * Add methods for each relevant endpoint in the API.
+ * 
+ * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
+ * 
+ */
+public class TwitterClient extends OAuthBaseClient {
+	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
+	public static final String REST_URL = "https://api.twitter.com/1.1/"; // Change this, base API URL
+	public static final String REST_CONSUMER_KEY = "qaJ75NFwgjrC0hC4BvtzhrSK7";
+	public static final String REST_CONSUMER_SECRET = "EOLyzQWriusrEr1NXYsSos9EwFwyaGHDNEsU4fB9AooWYJHWN3";
+	public static final String REST_CALLBACK_URL = "oauth://codepathtweets";
+
+	public TwitterClient(Context context) {
+		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+	}
+
+    public void getHomeTimeline(int page, AsyncHttpResponseHandler handler) {
+        String apiUrl = getApiUrl("statuses/home_timeline.json");
+        RequestParams params = new RequestParams();
+        params.put("count", "25");
+        client.get(apiUrl, params, handler);
+    }
+
+    public void postUpdate(String tweetBody, AsyncHttpResponseHandler handler) {
+        String apiUrl = getApiUrl("statuses/update.json");
+        RequestParams params = new RequestParams();
+        params.put("status", tweetBody);
+        client.post(apiUrl, params, handler);
+    }
+
+}
